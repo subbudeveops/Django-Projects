188edb446f2e0e088a781b02ba1923b52542a430
subbudeveops <subbureddam@gmail.com>
2022-02-08 15:52:19 +0530

    upload genatare pdf,csv and pagination

--- project2/myclub/urls.py
+++ project2/myclub/urls.py
@@ -6,19 +6,20 @@ from myclub import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.Month_Year_view,name='home'),
-    path('<int:year>/<str:month>/', views.Month_Year_view,name='home'),
-    path('event', views.Event_list,name='event_list'),
-    path('add_venue', views.add_venue,name='add-venue'),
-    path('list_venue', views.venue_list,name='list-venue'),
-    path('show_venue/<venue_id>/', views.show_venue,name='show-venue'),
-    path('search_venue', views.search_venue,name='search-venue'),
-    path('update_venue/<venue_id>/', views.update_venue,name='update-venue'),
-    path('update_event/<event_id>/', views.update_event,name='update-event'),
-    path('delete_event/<event_id>/', views.delete_event,name='delete-event'),
-    path('delete_venue/<venue_id>/', views.delete_venue,name='delete-venue'),
-    path('add_event', views.add_event,name='add-event'),
+    path('', views.Month_Year_view, name='home'),
+    path('<int:year>/<str:month>/', views.Month_Year_view, name='home'),
+    path('event', views.Event_list, name='event_list'),
+    path('add_venue', views.add_venue, name='add-venue'),
+    path('list_venue', views.venue_list, name='list-venue'),
+    path('show_venue/<venue_id>/', views.show_venue, name='show-venue'),
+    path('search_venue', views.search_venue, name='search-venue'),
+    path('update_venue/<venue_id>/', views.update_venue, name='update-venue'),
+    path('update_event/<event_id>/', views.update_event, name='update-event'),
+    path('delete_event/<event_id>/', views.delete_event, name='delete-event'),
+    path('delete_venue/<venue_id>/', views.delete_venue, name='delete-venue'),
+    path('add_event', views.add_event, name='add-event'),
+    path('venue_text', views.venue_text, name='venue-text'),
+    path('venue_csv', views.venue_csv, name='venue-csv'),
+    path('venue_pdf', views.venue_pdf, name='venue-pdf'),
+
 ]
-
-
-
--- project2/myclub/views.py
+++ project2/myclub/views.py
@@ -1,115 +1,195 @@
-from pickle import GET
+
 from django.forms import forms
 from django.shortcuts import redirect, render
 from django.http import HttpResponseRedirect
 import datetime
 import calendar
-from  calendar import Calendar, HTMLCalendar, month, monthcalendar 
+from calendar import Calendar, HTMLCalendar, month, monthcalendar
 from datetime import datetime
 from .models import Event, Venue
-from .forms import VenueForm,EventForm
+from .forms import VenueForm, EventForm
+from django.http import HttpResponse
+# import pdf attributes
+import csv
+import reportlab
+from django.http import FileResponse
+import io
+import reportlab
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+
+# import for create pagigations f
+from django.core.paginator import Paginator
+
+
 # Create your views here.
 
-def delete_venue(request,venue_id):
-  venue=Venue.obects.get(pk=venue_id)
-  venue.delete()
-  return redirect('list-venue')
-  
-  
-      
+# Generate odf file venuelist9
 
-def delete_event(request,event_id):
-  event=Event.objects.get(pk=event_id)
-  event.delete()
-  return redirect('event-list')
+
+def venue_pdf(request):
+    buff = io.BytesIO()
+    c = canvas.Canvas(buff, pagesize=letter, bottomup=0)
+    p = c.beginText()
+    p.setTextOrigin(inch, inch)
+    p.setFont("Helvetica", 14)
+    venues = Venue.objects.all()
+    lines = []
+    for venue in venues:
+        lines.append(venue.name)
+        lines.append(venue.address)
+        lines.append(venue.zip_code)
+        lines.append(venue.phone)
+        lines.append(venue.web)
+        lines.append(venue.email_address)
+        lines.append(
+            " == == == == == == == == == == == == == == == == == == == == == == == == == == ==")
+
+    for line in lines:
+        p.textLine(line)
+    c.drawText(p)
+    c.showPage()
+    c.save()
+    buff.seek(0)
+    return FileResponse(buff, as_attachment=True, filename='venue.pdf')
+
+
+def venue_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=venue.csv'
+    # create csv writer
+    writer = csv.writer(response)
+    venues = Venue.objects.all()
+    # Add Colum heading to the csv filename
+    writer.writerow(['Venue Name', 'Address', 'ZIP_CODE',
+                    'PhoneNo', 'Website', 'EmailId'])
+    for venue in venues:
+        writer.writerow(
+            [venue.name, venue.address, venue.zip_code, venue.phone, venue.web, venue.email_address])
+    return response
+
+
+def venue_text(request):
+    response = HttpResponse(content_type='text/plain')
+    response['Content-Disposition'] = 'attachment; filename = venue.txt'
+    venues = Venue.objects.all()
+    lines = []
+    for venue in venues:
+        lines.append(
+            f'{venue.name}\n{venue.address}\n{venue.zip_code}\n{venue.phone}\n{venue.web}\n{venue.email_address}\n\n\n')
+    # lines = ["This line one\n", "This is line 2\n"]
+    response.writelines(lines)
+    return response
+
+
+def delete_venue(request, venue_id):
+    venue = Venue.obects.get(pk=venue_id)
+    venue.delete()
+    return redirect('list-venue')
+
+
+def delete_event(request, event_id):
+    event = Event.objects.get(pk=event_id)
+    event.delete()
+    return redirect('event-list')
 
 
 def add_event(request):
-  submitted=False
-  
-  if request.method=="POST":
-    form=EventForm(request.POST)
+    submitted = False
+
+    if request.method == "POST":
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('./add_event?submitted=True')
+    else:
+        form = EventForm
+        if 'submitted' in request.GET:
+
+            submitted = True
+    return render(request, 'myclub/addevent.html', {'form': form, 'submitted': submitted})
+
+
+def update_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    form = VenueForm(request.POST or None, instance=venue)
     if form.is_valid():
-      form.save()
-      return HttpResponseRedirect('./add_event?submitted=True')
-  else:
-      form=EventForm
-      if 'submitted' in request.GET:
+        form .save()
+        return redirect('list-venue')
+    return render(request, 'myclub/updatevenue.html', {'venue': venue, 'form': form})
 
-        submitted=True
-  return render(request,'myclub/addevent.html',{'form':form,'submitted':submitted})
 
-def update_venue(request,venue_id):
-  venue=Venue.objects.get(pk=venue_id)
-  form=VenueForm(request.POST or None,instance=venue)
-  if form.is_valid():
-    form .save()
-    return redirect('list-venue')
+def update_event(request, event_id):
+    event = Event.objects.get(pk=event_id)
+    form = EventForm(request.POST or None, instance=event)
+    if form.is_valid():
+        form.save()
+        return redirect('event_list')
+    return render(request, 'myclub/update_event.html', {'form': form, 'event': event})
 
-  return render(request,'myclub/updatevenue.html',{'venue':venue,'form':form})
 
-def update_event(request,event_id):
-  event=Event.objects.get(pk=event_id)
-  form=EventForm(request.POST or None, instance=event)
-  if form.is_valid():
-    form.save()
+def delete_event(request, event_id):
+    event = Event.objects.get(pk=event_id)
+    event.delete()
     return redirect('event_list')
-  return render(request,'myclub/update_event.html',{'form':form,'event':event})
 
-def delete_event(request,event_id):
-  event=Event.objects.get(pk=event_id)
-  event.delete()
-  return redirect('event_list')
 
 def search_venue(request):
-  if request.method=="POST":
-    searchvenue=request.POST['searchvenue']
-    venues=Venue.objects.filter(name__contains=searchvenue)
-    return render(request,'myclub/searchvenue.html',{'searchvenue':searchvenue,'venues':venues})
+    if request.method == "POST":
+        searchvenue = request.POST['searchvenue']
+        venues = Venue.objects.filter(name__contains=searchvenue)
+        return render(request, 'myclub/searchvenue.html', {'searchvenue': searchvenue, 'venues': venues})
+    else:
+        return render(request, 'myclub/searchvenue.html', {})
 
 
-  else:
-    return render(request,'myclub/searchvenue.html',{})
+def show_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    return render(request, 'myclub/showvenue.html', {'venue': venue})
 
-
-
-def show_venue(request,venue_id):
-  venue=Venue.objects.get(pk=venue_id)
-  return render(request,'myclub/showvenue.html',{'venue':venue})
 
 def venue_list(request):
-  venue_list=Venue.objects.all().order_by('name')
-  return render(request,'myclub/venuelist.html',{'venue_list':venue_list})
+    #venue_list = Venue.objects.all().order_by('name')
+    venue_list = Venue.objects.all()
+    # setup pagenation for venue
+    p = Paginator(Venue.objects.all(), 1)
+    page = request.GET.get('page',)
+    venues = p.get_page(page)
+    return render(request, 'myclub/venuelist.html', {'venue_list': venue_list, 'venues': venues})
+
 
 def add_venue(request):
-  form=VenueForm()
-  if request.method=="POST":
-    form=VenueForm(request.POST)
-    if form.is_valid():
-      form.save()
-    return Month_Year_view(request)
-  return render(request,'myclub/add_venue.html',{'form':form})
+    form = VenueForm()
+    if request.method == "POST":
+        form = VenueForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return Month_Year_view(request)
+    return render(request, 'myclub/add_venue.html', {'form': form})
+
 
 def Event_list(request):
-  all_list=Event.objects.all().order_by('name')
-  return render(request,'myclub/event_list.html',{'all_list':all_list})
+    all_list = Event.objects.all().order_by('name')
+    return render(request, 'myclub/event_list.html', {'all_list': all_list})
 
-def Month_Year_view(request,year=datetime.now().year,month=datetime.now().strftime('%B')):
-  name='subbareddy' 
-  month=month.capitalize()
-  #convert month from month name to number
-  month_number=list(calendar.month_name).index(month)
-  cal=HTMLCalendar().formatmonth(year,month_number)
-  now=datetime.now()
-  current_year=now.year
-  time=now.strftime("%I:%M:%S %p")
-  return render(request,'myclub/home.html',{
-                          'name':name,
-                          'year':year,
-                          'month':month,
-                          'month_number':month_number,
-                          'cal':cal,
-                          'now':now,
-                          'current_year':current_year,
-                          'time':time
-                          })
+
+def Month_Year_view(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    name = 'subbareddy'
+    month = month.capitalize()
+    # convert month from month name to number
+    month_number = list(calendar.month_name).index(month)
+    cal = HTMLCalendar().formatmonth(year, month_number)
+    now = datetime.now()
+    current_year = now.year
+    time = now.strftime("%I:%M:%S %p")
+    return render(request, 'myclub/home.html', {
+        'name': name,
+        'year': year,
+        'month': month,
+        'month_number': month_number,
+        'cal': cal,
+        'now': now,
+        'current_year': current_year,
+        'time': time
+    })
