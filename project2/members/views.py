413d8ada4775a5c328afe795d295f943feb03620
subbudeveops <subbureddam@gmail.com>
2022-02-12 16:26:22 +0530

    ipdated the  django 26 is completed

--- project2/members/views.py
+++ project2/members/views.py
@@ -3,14 +3,14 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
-
+from .forms import RegisterUserForm
 # Create your views here.
 
 
 def login_user(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get['username']
+        password = request.POST.get['password']
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
@@ -30,22 +30,20 @@ def logout_user(request):
     logout(request)
     messages.success(
         request, ('There was loggedout '))
-    return redirect('list-venue')
+    return redirect('home')
 
 
 def register_user(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterUserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data["username"]
-            password = form.cleaned_data["password"]
+            password = form.cleaned_data["password1"]
             user = authenticate(request, username=username, password=password)
             login(request, user)
             messages.success(request, ('Register successfully'))
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = RegisterUserForm()
     return render(request, 'registration/register.html', {'form': form})
-
-    return render(request, 'registration/register.html', {})
